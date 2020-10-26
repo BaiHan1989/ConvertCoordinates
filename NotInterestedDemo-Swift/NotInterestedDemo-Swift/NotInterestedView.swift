5f7796b9e0e9241c731cfb504590f32d5e7363c7
Lazyload <baih_1989@163.com>
2020-10-26 17:02:28 +0800

    点击不敢兴趣按钮弹窗

--- NotInterestedDemo-Swift/NotInterestedDemo-Swift/NotInterestedView.swift
+++ NotInterestedDemo-Swift/NotInterestedDemo-Swift/NotInterestedView.swift
@@ -12,41 +12,60 @@ class NotInterestedView: UIView {
     private var backgroundView: UIView = UIView()
     /// 删除视图
     private var deleteView: UIView = UIView()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupSubviews()
     }
-    
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundView.frame = self.bounds
-        
-        deleteView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        backgroundView.frame = bounds
     }
-    
-    func show() {
+
+    func show(at point: CGPoint) {
         // 获取到keyWindow
         let window = UIApplication.shared.keyWindow
         window?.addSubview(self)
-    }
 
+        deleteView.frame = CGRect(x: point.x, y: point.y + 40, width: 0, height: 0)
+
+        UIView.animate(withDuration: 0.3) {
+            self.deleteView.frame = CGRect(x: 15, y: point.y + 40, width: UIScreen.main.bounds.width - 30, height: 200)
+        }
+    }
 }
 
-
+/// UI
 extension NotInterestedView {
     // MARK: - UI
+
     func setupSubviews() {
         backgroundView.backgroundColor = UIColor.black
-        backgroundView.alpha = 0.5
+        backgroundView.alpha = 0.3
         addSubview(backgroundView)
-        
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismiss))
+        backgroundView.addGestureRecognizer(tapGesture)
+
         deleteView.backgroundColor = UIColor.orange
         addSubview(deleteView)
+    }
+}
+
+/// 响应事件
+extension NotInterestedView {
+    // MARK: - Action
+    @objc func dismiss() {
+        UIView.animate(withDuration: 0.3) {
+            self.deleteView.frame = CGRect(x: UIScreen.main.bounds.width - 15, y: self.deleteView.frame.origin.y, width: 0, height: 0)
+        } completion: { (finished) in
+            self.removeFromSuperview()
+        }
+
     }
 }
--- NotInterestedDemo-Swift/NotInterestedDemo-Swift/ViewController.swift
+++ NotInterestedDemo-Swift/NotInterestedDemo-Swift/ViewController.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NotInterestedCellDelegate {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        
     }
-    
+
     func setupUI() {
         let tableView = UITableView(frame: view.bounds)
         tableView.rowHeight = 200
@@ -24,28 +22,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         view.addSubview(tableView)
     }
 
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
 
-    
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! NotInterestedCell
-        
+
         cell.delegate = self
-        
+
         return cell
     }
 
     func notInterestedCellDeleteButtonClick(_ cell: NotInterestedCell, _ deleteButton: UIButton) {
-    
+        
+        // 获取不感兴趣按钮的位置
+        let point = cell.convert(deleteButton.frame.origin, to: nil)
         
         // 创建不感兴趣视图
         let notInterestedView = NotInterestedView(frame: view.bounds)
-        notInterestedView.show()
+        notInterestedView.show(at: point)
     }
-
 }
-
